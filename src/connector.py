bdb249f2645c61769f3be5664e5e1267cda537f6
cybersheepdog <jmwiedner@gmail.com>
2025-11-12 16:12:47 +0000

    Updated. Fixed loop to pick up where left off on indicator insertion.

--- src/connector.py
+++ src/connector.py
@@ -21,11 +21,10 @@ warnings.filterwarnings('ignore')
 ipv4_regex = "^(\d{3}\.?){4}$"
 ipv6_regex = "^([a-fA-f0-9]{0,4}:?){8}$"
 
-def add_c2tracker_ips(current_c2_tracker_ips, SELECTED_TLP):
-    ip_count = 0
-    opencti_labels = get_labels()
+def add_c2tracker_ips(opencti_labels, current_c2_tracker_ips, c2_ip_count, ip_count, inserted_ips, SELECTED_TLP):    
     for item in current_c2_tracker_ips:
         for c2, ips in item.items():
+            print(f"Adding IPs for {c2}:")
             created_labels = []
             if c2 == "Cobalt Strike C2":
                 labels = ["Cobalt Strike", "C2"]
@@ -48,6 +47,7 @@ def add_c2tracker_ips(current_c2_tracker_ips, SELECTED_TLP):
                 l = l.lower()
                 matches = [item_label for item_label in opencti_labels if l in item_label.values()]
                 if len(matches) == 0:
+                    #print(f"Creating new label for {l}")
                     random_color = generate_random_color()
                     label = opencti_api_client.label.create(
                         value=l,
@@ -55,71 +55,72 @@ def add_c2tracker_ips(current_c2_tracker_ips, SELECTED_TLP):
                         )
                     created_labels.append(label)
                 else:
+                    #print("Label already exists.")
                     label = matches[0]
                     created_labels.append(label)
             for ip in ips:
-                ip_count += 1
-                if re.search(ipv6_regex, ip):
-                    # Create IPv6 indicator
-                    observable = opencti_api_client.stix_cyber_observable.create(
-                        observableData={
-                            "type": "IPv6-Addr",
-                            "value": f"{ip}",
-                            "x_opencti_description": f"This IP is was recently seen hosting {c2}",
-                            "x_opencti_score": 100,
-                            "x_opencti_create_indicator":True
-                            }, objectMarking=[SELECTED_TLP["id"]]
-                        )
-                    # Get the observable ID to add labels
-                    observable_id = observable['id']
-                    # Get the indicator ID to add labels
-                    indicator_id = observable['indicatorsIds'][0]
-
-                    for label in created_labels:
-                        # Add label to indicator
-                        opencti_api_client.stix_domain_object.add_label(id=indicator_id, label_id=label["id"])
-                        opencti_api_client.stix_cyber_observable.add_label(id=observable_id, label_id=label["id"])
-                else:# re.search(ipv4_regex, ip):
-                    # Create IPv4 indicator
-                    observable = opencti_api_client.stix_cyber_observable.create(
-                        observableData={
-                            "type": "IPv4-Addr",
-                            "value": f"{ip}",
-                            "x_opencti_description": f"This IP is was recently seen hosting {c2}",
-                            "x_opencti_score": 100,
-                            "x_opencti_create_indicator":True
-                            }, objectMarking=[SELECTED_TLP["id"]]
-                        )
-                    # Get the observable ID to add labels
-                    observable_id = observable['id']
-                    # Get the indicator ID to add labels
-                    indicator_id = observable['indicatorsIds'][0]
-
-                    for label in created_labels:
-                        # Add label to indicator
-                        opencti_api_client.stix_domain_object.add_label(id=indicator_id, label_id=label["id"])
-                        opencti_api_client.stix_cyber_observable.add_label(id=observable_id, label_id=label["id"])
-
-def check_mitre():
-    mitre = False
-    data = {"pagination": {"hasNextPage": True, "endCursor": None}}
-    while data["pagination"]["hasNextPage"] and mitre == False:
-        after = data["pagination"]["endCursor"]
-        if after:
-            print("Listing indicators after " + after)
-        data = opencti_api_client.malware.list(
-            first=50,
-            after=after,
-            withPagination=True,
-            orderBy="created_at",
-            orderMode="asc",
-        )
-        for malware in data["entities"]:
-            if str(malware["createdBy"]["name"]) == "The MITRE Corporation":
-                mitre = True
-                print("MITRE is enabled")
-                break
-    return mitre
+                #print(f"\tAdding {ip}")
+                pair = {ip:c2}
+                if pair in inserted_ips:
+                    #print(f"\t{ip}:{c2} was already inserted into opencti.  Moving on.")
+                    pass
+                else:
+                    if re.search(ipv6_regex, ip):
+                        # Create IPv6 indicator
+                        observable = opencti_api_client.stix_cyber_observable.create(
+                            observableData={
+                                "type": "IPv6-Addr",
+                                "value": f"{ip}",
+                                "x_opencti_description": f"This IP is was recently seen hosting {c2}",
+                                "x_opencti_score": 100,
+                                "x_opencti_create_indicator":True
+                                }, objectMarking=[TLP_AMBER_CTI["id"]]
+                            )
+                        # Get the observable ID to add labels
+                        observable_id = observable['id']
+                        #print(f"Observable ID: {observable_id}")
+                        # Get the indicator ID to add labels
+                        indicator_id = observable['indicatorsIds'][0]
+                        #print(f"Indicator ID: {indicator_id}")
+                        
+                        for label in created_labels:
+                            # Add label to indicator
+                            #print(f"Label: {label}")
+                            opencti_api_client.stix_domain_object.add_label(id=indicator_id, label_id=label["id"])
+                            opencti_api_client.stix_cyber_observable.add_label(id=observable_id, label_id=label["id"])
+                        # Keeps track of the C2:IP Combos that have already been inserted to pick back up if there is a time out
+                        inserted = {ip:c2}
+                        inserted_ips.append(inserted)
+                        ip_count += 1
+                    else:# re.search(ipv4_regex, ip):
+                        # Create IPv4 indicator
+                        observable = opencti_api_client.stix_cyber_observable.create(
+                            observableData={
+                                "type": "IPv4-Addr",
+                                "value": f"{ip}",
+                                "x_opencti_description": f"This IP is was recently seen hosting {c2}",
+                                "x_opencti_score": 100,
+                                "x_opencti_create_indicator":True
+                                }, objectMarking=[TLP_AMBER_CTI["id"]]
+                            )
+                        # Get the observable ID to add labels
+                        observable_id = observable['id']
+                        #print(f"Observable ID: {observable_id}")
+                        # Get the indicator ID to add labels
+                        indicator_id = observable['indicatorsIds'][0]
+                        #print(f"Indicator ID: {indicator_id}")
+                        
+                        for label in created_labels:
+                            # Add label to indicator
+                            #print(f"Labe: {label}")
+                            opencti_api_client.stix_domain_object.add_label(id=indicator_id, label_id=label["id"])
+                            opencti_api_client.stix_cyber_observable.add_label(id=observable_id, label_id=label["id"])
+                        # Keeps track of the C2:IP Combos that have already been inserted to pick back up if there is a time out
+                        inserted = {ip:c2}
+                        inserted_ips.append(inserted)
+                        ip_count += 1
+                    #else:
+                       # print(f"IP did not match a regex:\t{ip}") 
 
 def create_relationships(SELECTED_TLP, indicators, tools):
     # Create the tag (if not exists)
@@ -169,6 +170,26 @@ def create_relationships(SELECTED_TLP, indicators, tools):
                             )
                             # Add label to relationship
                             opencti_api_client.stix_core_relationship.add_label(id=relationship["id"], label_id=label["id"])
+def check_mitre():
+    mitre = False
+    data = {"pagination": {"hasNextPage": True, "endCursor": None}}
+    while data["pagination"]["hasNextPage"] and mitre == False:
+        after = data["pagination"]["endCursor"]
+        if after:
+            print("Listing indicators after " + after)
+        data = opencti_api_client.malware.list(
+            first=50,
+            after=after,
+            withPagination=True,
+            orderBy="created_at",
+            orderMode="asc",
+        )
+        for malware in data["entities"]:
+            if str(malware["createdBy"]["name"]) == "The MITRE Corporation":
+                mitre = True
+                print("MITRE is enabled")
+                break
+    return mitre
 
 def generate_random_color():
     return '#{:06x}'.format(random.randint(0, 0xFFFFFF))
@@ -234,6 +255,7 @@ def get_current_indicators():
     print("Finished gathering current indicators")
     return final_indicators
 
+
 def get_labels():
     final_labels = []
     max_retries = 3
@@ -242,6 +264,8 @@ def get_labels():
         after = data["pagination"]["endCursor"]
         if after:
             print("Listing labels after " + after)
+        #for attempt in range(max_retries):
+            #try:
         data = opencti_api_client.label.list(
             first=50,
             after=after,
@@ -250,6 +274,10 @@ def get_labels():
             orderMode="asc",
         )
         final_labels += data["entities"]
+        #        break
+        #    except:
+        #        print(f"Timeout occurred, retrying... ({attempt + 1})")
+        #        sleep(5)  # Wait before retrying
     
     return final_labels
 
@@ -267,9 +295,8 @@ def get_malware():
             orderBy="created_at",
             orderMode="asc",
         )
-        print("Finished getting malware")
         final_malware += data["entities"]
-
+    
     return final_malware
 
 def get_tools():
@@ -278,7 +305,7 @@ def get_tools():
     while data["pagination"]["hasNextPage"]:
         after = data["pagination"]["endCursor"]
         if after:
-            print("Listing tools after " + after)
+            print("Listing indicators after " + after)
         data = opencti_api_client.tool.list(
             first=50,
             after=after,
@@ -286,10 +313,19 @@ def get_tools():
             orderBy="created_at",
             orderMode="asc",
         )
-        print("Finished getting Tools")
         final_tools += data["entities"]
-
+    
     return final_tools
+
+def loop(opencti_labels, current_c2_tracker_ips, c2_ip_count, ip_count, inserted_ips, SELECTED_TLP):
+    try:
+        add_c2tracker_ips(opencti_labels, current_c2_tracker_ips, c2_ip_count, ip_count, inserted_ips, SELECTED_TLP)
+    except Exception as e:
+        print("[+] Main Loop Failed. Restarting in 10 seconds.")
+        print(e)
+        sleep(20)
+        loop(opencti_labels, current_c2_tracker_ips, c2_ip_count, ip_count, inserted_ips, SELECTED_TLP)
+
 
 def main():
     api_url = os.getenv("OPENCTI_BASE_URL") + "/graphql"
@@ -319,14 +355,42 @@ def main():
         malware = get_malware()
         create_relationships(SELECTED_TLP, indicators, tools=malware)
 
-def loop():
-    try:
-        main()
-    except Exception as e:
-        print("[+] Main Loop Failed. Restarting in 10 seconds.")
-        print(e)
-        sleep(10)
-        loop()
 
 if __name__ == "__main__":
-    loop()
+    api_url = os.getenv("OPENCTI_BASE_URL") + "/graphql"
+    api_token = os.getenv("CONNECTOR_IMPORT_C2_TRACKER")
+    opencti_api_client = OpenCTIApiClient(api_url, api_token)
+    # Get TLP ID's
+    TLP_WHITE_CTI = opencti_api_client.marking_definition.read(id=TLP_WHITE["id"])
+    SELECTED_TLP = TLP_WHITE_CTI
+
+    #TLP_GREEN_CTI = opencti_api_client.marking_definition.read(id=TLP_GREEN["id"])
+    #SELECTED_TLP = TLP_GREEN_CTI
+
+    #TLP_AMBER_CTI = opencti_api_client.marking_definition.read(id=TLP_AMBER["id"])
+    #SELECTED_TLP = TLP_AMBER_CTI
+
+    #TLP_RED_CTI = opencti_api_client.marking_definition.read(id=TLP_RED["id"])
+    #SELECTED_TLP = TLP_RED_CTI
+
+    opencti_labels = get_labels()
+    current_c2_tracker_ips = get_current_c2_tracker_ips()
+    # get count of C2's in order to find place if connection is reset
+    c2_ip_count = 0
+    ip_count = 0
+    for item in current_c2_tracker_ips:
+        for c2, ips in item.items():
+            for ip in ips:
+                c2_ip_count += 1
+    # Empty list to hold IPS that have been inserted
+    inserted_ips = []
+    while len(inserted_ips) < c2_ip_count:
+        loop(opencti_labels, current_c2_tracker_ips, c2_ip_count, ip_count, inserted_ips, SELECTED_TLP)
+
+    mitre = check_mitre()
+    if mitre:
+        indicators = get_current_indicators()
+        tools = get_tools()
+        create_relationships(SELECTED_TLP, indicators, tools)
+        malware = get_malware()
+        create_relationships(SELECTED_TLP, indicators, tools=malware)
